1135478fffa12c5f249266a297d1f4c367cbffa7
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-06-22 15:24:50 -0400

    making node.js server to serve this

--- server.js
+++ server.js
@@ -17,6 +17,38 @@ function parseQuery (qstr) {
     return query;
 }
 
+function run(query) {
+    var command = 'build/bin/./tangramPaparazzi';
+    if (query['lat'] && typeof parseFloat(query['lat']) === 'number') {
+        command += ' -lat ' + query['lat'];
+    }
+    if (query['lon'] && typeof parseFloat(query['lon']) === 'number') {
+        command += ' -lon ' + query['lon'];
+    }
+    if (query['zoom'] && typeof parseFloat(query['zoom']) === 'number') {
+        command += ' -z ' + query['zoom'];
+    }
+    if (query['tilt'] && typeof parseFloat(query['tilt']) === 'number') {
+        command += ' -t ' + query['tilt'];
+    }
+    if (query['rot'] && typeof parseFloat(query['rot']) === 'number') {
+        command += ' -r ' + query['rot'];
+    }
+    if (query['width'] && typeof query['width'] === 'number') {
+        command += ' -w ' + query['width'];
+    }
+    if (query['height'] && typeof query['heihgt'] === 'number') {
+        command += ' -h ' + query['height'];
+    }
+    command += ' -o out.png';
+    console.log('Command',command);
+    exec(command);
+    console.log('Done');
+    var img = fs.readFileSync('out.png');
+    res.writeHead(200, {'Content-Type': 'image/png' });
+    res.end(img, 'binary');
+}
+
 var server = http.createServer( function (req, res) {
     var request = url.parse(req.url);
 
@@ -24,42 +56,16 @@ var server = http.createServer( function (req, res) {
 
     if (request.query) {
         var query = parseQuery(request.query);
-
         console.log('Query', query);
-
-        var command = 'build/bin/./tangramPaparazzi';
-        if (query['lat'] && typeof parseFloat(query['lat']) === 'number') {
-            command += ' -lat ' + query['lat'];
-        }
-        if (query['lon'] && typeof parseFloat(query['lon']) === 'number') {
-            command += ' -lon ' + query['lon'];
-        }
-        if (query['zoom'] && typeof parseFloat(query['zoom']) === 'number') {
-            command += ' -z ' + query['zoom'];
-        }
-        if (query['tilt'] && typeof parseFloat(query['tilt']) === 'number') {
-            command += ' -t ' + query['tilt'];
-        }
-        if (query['rot'] && typeof parseFloat(query['rot']) === 'number') {
-            command += ' -r ' + query['rot'];
-        }
-    
         if (query['scene']) {
             console.log(query['scene']);
+            var file = fs.createWriteStream("scene.yaml");
+            var request = http.get(query['scene'], function (response) {
+                response.pipe(file);
+                run(query);
+            });
+        } else {
+            run(query);
         }
-
-        if (query['width'] && typeof query['width'] === 'number') {
-            command += ' -w ' + query['width'];
-        }
-        if (query['height'] && typeof query['heihgt'] === 'number') {
-            command += ' -h ' + query['height'];
-        }
-        command += ' -o out.png';
-        console.log('Command',command);
-        exec(command);
-        console.log('Done');
-        var img = fs.readFileSync('out.png');
-        res.writeHead(200, {'Content-Type': 'image/png' });
-        res.end(img, 'binary');
     }
 }).listen(HTTP_PORT);
