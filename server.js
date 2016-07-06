6e61a3f2a8179fb137f1f10df923920cf56cb9f4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-07-06 09:32:25 -0400

    changing order

--- server.js
+++ server.js
@@ -8,18 +8,6 @@ var http = require('http'),   // http Server
 var BIN = 'build/bin/paparazzi';
 var HTTP_PORT = 8080;
 
-fs.readFile('/etc/os-release', 'utf8', function (err,data) {
-    if (err) {
-        return console.log(err);
-    }
-    if (data.startsWith('NAME="Amazon Linux AMI"')) {
-        HTTP_PORT = 80;
-        BIN = 'export DISPLAY=:0 && ' + BIN;
-        console.log('Running on Amazon Linux GPU HeadLess Server at port', HTTP_PORT);
-    }
-
-});
-
 function parseQuery (qstr) {
     var query = {};
     var a = qstr.split('&');
@@ -43,47 +31,57 @@ function download(url, dest, cb) {
     });
 };
 
-var server = http.createServer( function (req, res) {
-    var request = url.parse(req.url);
-        
-    console.log('Request', request);
-
-    if (request.query) {
-        var query = parseQuery(request.query);
-        console.log('Query', query);
-
-        var command = BIN;
-       
-        if (query['lat'] && typeof parseFloat(query['lat']) === 'number') {
-            command += ' -lat ' + query['lat'];
-        }
-        if (query['lon'] && typeof parseFloat(query['lon']) === 'number') {
-            command += ' -lon ' + query['lon'];
-        }
-        if (query['zoom'] && typeof parseFloat(query['zoom']) === 'number') {
-            command += ' -z ' + query['zoom'];
-        }
-        if (query['tilt'] && typeof parseFloat(query['tilt']) === 'number') {
-            command += ' -t ' + query['tilt'];
-        }
-        if (query['rot'] && typeof parseFloat(query['rot']) === 'number') {
-            command += ' -r ' + query['rot'];
-        }
-        if (query['width'] && typeof parseFloat(query['width']) === 'number') {
-            command += ' -w ' + query['width'];
-        }
-        if (query['height'] && typeof parseFloat(query['height']) === 'number') {
-            command += ' -h ' + query['height'];
-        }
-        if (query['scene']) {
-            command += ' -s ' + query['scene'];
-        }
-        command += ' -o out.png';
-        console.log(command);
-        exec(command);
-        console.log('Done');
-        var img = fs.readFileSync('out.png');
-        res.writeHead(200, {'Content-Type': 'image/png' });
-        res.end(img, 'binary');
+fs.readFile('/etc/os-release', 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
     }
-}).listen(HTTP_PORT);
+    if (data.startsWith('NAME="Amazon Linux AMI"')) {
+        HTTP_PORT = 80;
+        BIN = 'export DISPLAY=:0 && ' + BIN;
+        console.log('Running on Amazon Linux GPU HeadLess Server at port', HTTP_PORT);
+    }
+    var server = http.createServer( function (req, res) {
+        var request = url.parse(req.url);
+            
+        console.log('Request', request);
+
+        if (request.query) {
+            var query = parseQuery(request.query);
+            console.log('Query', query);
+
+            var command = BIN;
+           
+            if (query['lat'] && typeof parseFloat(query['lat']) === 'number') {
+                command += ' -lat ' + query['lat'];
+            }
+            if (query['lon'] && typeof parseFloat(query['lon']) === 'number') {
+                command += ' -lon ' + query['lon'];
+            }
+            if (query['zoom'] && typeof parseFloat(query['zoom']) === 'number') {
+                command += ' -z ' + query['zoom'];
+            }
+            if (query['tilt'] && typeof parseFloat(query['tilt']) === 'number') {
+                command += ' -t ' + query['tilt'];
+            }
+            if (query['rot'] && typeof parseFloat(query['rot']) === 'number') {
+                command += ' -r ' + query['rot'];
+            }
+            if (query['width'] && typeof parseFloat(query['width']) === 'number') {
+                command += ' -w ' + query['width'];
+            }
+            if (query['height'] && typeof parseFloat(query['height']) === 'number') {
+                command += ' -h ' + query['height'];
+            }
+            if (query['scene']) {
+                command += ' -s ' + query['scene'];
+            }
+            command += ' -o out.png';
+            console.log(command);
+            exec(command);
+            console.log('Done');
+            var img = fs.readFileSync('out.png');
+            res.writeHead(200, {'Content-Type': 'image/png' });
+            res.end(img, 'binary');
+        }
+    }).listen(HTTP_PORT);
+});
