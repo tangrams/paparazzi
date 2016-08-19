4167afea2c687a41378cee2bdb6d0df2944ce622
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-08-19 14:51:52 -0400

    98pct in OSX

--- src/platform_headless.h
+++ src/platform_headless.h
@@ -2,16 +2,6 @@
 
 #include "platform.h"
 
-#ifdef PLATFORM_LINUX
-#define GL_GLEXT_PROTOTYPES
-#include <GLFW/glfw3.h>
-#endif
-
-#ifdef PLATFORM_OSX
-#define GLFW_INCLUDE_GLEXT
-#include <GLFW/glfw3.h>
-#endif
-
 #ifdef PLATFORM_RPI
 #include "bcm_host.h"
 
@@ -19,6 +9,18 @@
 #include <GLES2/gl2ext.h>
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
+#else
+#define GL_GLEXT_PROTOTYPES
+#include <GLFW/glfw3.h>
+// #define GLFW_INCLUDE_GLEXT
+#endif
+
+#ifdef PLATFORM_OSX
+#define glClearDepthf glClearDepth
+#define glDepthRangef glDepthRange
+#define glDeleteVertexArrays glDeleteVertexArraysAPPLE
+#define glGenVertexArrays glGenVertexArraysAPPLE
+#define glBindVertexArray glBindVertexArrayAPPLE
 #endif
 
 void processNetworkQueue();
