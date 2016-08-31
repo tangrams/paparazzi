809af3b19c21fb5c5f7d376d91c5c1d12f44a89f
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-08-31 12:55:12 -0400

    debugging

--- src/paparazzi.cpp
+++ src/paparazzi.cpp
@@ -197,8 +197,11 @@ void Paparazzi::update () {
     bool bFinish = false;
     while (delta < MAX_WAITING_TIME && !bFinish) {
         // Update Network Queue
+        LOG("start processNetWorkQueue");
         processNetworkQueue();
+        LOG("start map update");
         bFinish = m_map->update(10.);
+        LOG("check time");
         delta = getTime() - startTime;
     }
     LOG("FINISH");
