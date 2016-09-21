bc13e02c72cf3a603c68cd5c9b5a21c04ec25b78
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-09-21 09:30:43 -0400

    only one fbo for rpi

--- src/tools/aab.cpp
+++ src/tools/aab.cpp
@@ -11,8 +11,9 @@
 #include "stb_image_write.h"
 
 AntiAliasedBuffer::AntiAliasedBuffer() : 
+                                        m_fbo_in(nullptr),
 #ifndef PLATFORM_RPI
-                                        m_fbo_in(nullptr), m_fbo_out(nullptr), 
+                                        m_fbo_out(nullptr), 
 #endif
                                         m_shader(nullptr), m_vbo(0), m_width(0), m_height(0), m_scale(2.) {
 
@@ -63,9 +64,7 @@ AntiAliasedBuffer::~AntiAliasedBuffer() {
 }
 
 void AntiAliasedBuffer::bind() {
-#ifndef PLATFORM_RPI
     m_fbo_in->bind();
-#endif
     Tangram::GL::viewport(0.0f, 0.0f, m_width*m_scale, m_height*m_scale);
     Tangram::GL::clearColor(0.0f, 0.0f, 0.0f, 1.0f);
     Tangram::GL::clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -81,13 +80,14 @@ void AntiAliasedBuffer::setSize(const unsigned int &_width, const unsigned int &
     if (_width != m_width || _height != m_height) {
         m_width = _width;
         m_height = _height;
-#ifndef PLATFORM_RPI
+
         if (!m_fbo_in) {
             m_fbo_in = std::unique_ptr<Fbo>(new Fbo(m_width*m_scale, m_height*m_scale));
         } else {
             m_fbo_in->resize(m_width*m_scale, m_height*m_scale);
         }
-        
+
+#ifndef PLATFORM_RPI
         if (!m_fbo_out) {
             m_fbo_out = std::unique_ptr<Fbo>(new Fbo(m_width, m_height, false));
         } else {
@@ -132,5 +132,7 @@ void AntiAliasedBuffer::getPixelsAsString(std::string &_image) {
 
 #ifndef PLATFORM_RPI
     m_fbo_out->unbind();
+#elif
+    m_fbo_in->unbind();
 #endif
 }
